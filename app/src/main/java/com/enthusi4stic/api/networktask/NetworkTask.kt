876f7b2b25ef7c9814f444412e42ee3b9c2952ca
Mohsen Dehbashi <dehbashi@soc1.ir>
2019-09-16 15:51:22 +0430

    headers added to NetworkTask.

--- app/src/main/java/com/enthusi4stic/api/networktask/NetworkTask.kt
+++ app/src/main/java/com/enthusi4stic/api/networktask/NetworkTask.kt
@@ -53,7 +53,8 @@ open class NetworkTask(
     val method: Method = Method.GET,
     protected val body: RequestBody? = null,
     protected val ctx: Context? = null,
-    val waitingMessage: String? = null
+    val waitingMessage: String? = null,
+    vararg val headers: Pair<String, String>
 ) {
     enum class Method {
         GET, POST, PUT, PATCH, DELETE, HEAD
@@ -107,6 +108,10 @@ open class NetworkTask(
                     Method.PUT -> it.put(body!!)
                     Method.PATCH -> it.patch(body!!)
                     Method.HEAD -> it.head()
+                }
+            }.also {
+                headers.forEach {header ->
+                    it.addHeader(header.first, header.second)
                 }
             }.build()
             OkHttpClient().newCall(request).execute()
